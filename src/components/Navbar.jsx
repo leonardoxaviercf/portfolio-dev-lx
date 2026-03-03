b752c7c66ce98bddbbdc7181c29ff8785564e128
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-03 20:17:14 -0300

    Corrigindo navbar: Adição de menu hamburguer para melhor experiência em mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
+
   return (
     <header>
-      <nav>
+      <nav className="nav-container">
         <div className="logo">LX<span>.</span></div>
-        <ul>
-          <li><a href="#sobre">Sobre Mim</a></li>
-          <li><a href="#tecnologias">Tecnologias</a></li>
-          <li><a href="#projetos">Projetos</a></li>
-          <li><a href="#contato">Contato</a></li>
+        
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
+        <ul className={isMenuOpen ? "nav-links active" : "nav-links"}>
+          <li><a href="#sobre" onClick={closeMenu}>Sobre Mim</a></li>
+          <li><a href="#tecnologias" onClick={closeMenu}>Tecnologias</a></li>
+          <li><a href="#projetos" onClick={closeMenu}>Projetos</a></li>
+          <li><a href="#contato" onClick={closeMenu}>Contato</a></li>
         </ul>
       </nav>
     </header>
